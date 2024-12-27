75af6b03184df88c3dddecb026c78134d95ecc3e
zlnc <collinsolafusi@gmail.com>
2024-12-27 02:12:09 +0000

    Initial commit defining project structure and running server

---
+++ server/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env") //Access environment variables
+	if err != nil {
+		log.Fatalf("Error loading environment variables: %s", err)
+	}
+
+	serverPort := os.Getenv("SERVER_PORT") //Access server port
+
+	router := mux.NewRouter() //Create a new router
+
+	log.Println("Server is running")
+	err = http.ListenAndServe(fmt.Sprintf(":%s", serverPort), router) //This will run the server and listen for requests
+	if err != nil { 
+		log.Fatalln("Error occured in server: %s", err)
+	}
+
+}
---
+++ server/internal/database/models.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"log"
+
+	_ "github.com/lib/pq"
+	_ "gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"unique"`
+	Password string 
+}
+
+func createUserDB(db *gorm.DB, username string, password string) {
+	newRow := User{Username:username, Password:password}
+	err := db.Model(&User{}).Create(&newRow).Error
+	if err != nil {
+		log.Println("Error in creating new user due to: %s", err)
+	}
+}
